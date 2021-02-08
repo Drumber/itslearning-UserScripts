0f8e179b24f7f7112c2349ce358a97c346536193
Lars <drumberhd@gmail.com>
2021-02-08 10:33:09 +0100

    added UserScript and updated README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # itslearning-UserScripts
-UserScripts to improve the experience on itslearning
+UserScripts to improve the experience on itslearning.
+- ImagesInChat: adds a image preview to the chat
+
+## Installtion
+1. Install [Tampermonkey for Firefox](https://tampermonkey.net/?ext=dhdg&browser=firefox), [Chrome](https://tampermonkey.net/?ext=dhdg&browser=chrome), [Opera](https://tampermonkey.net/?ext=dhdg&browser=opera)or [Safari](https://tampermonkey.net/?ext=dhdg&browser=safari).
+2. Install [itslearning-ImagesInChat.user.js](https://raw.githubusercontent.com/Drumber/itslearning-UserScripts/master/itslearning-ImagesInChat.user.js)
---
+++ itslearning-ImagesInChat.user.js
@@ -0,0 +1,201 @@
+// ==UserScript==
+// @name         ImagesInChat
+// @namespace    https://github.com/Drumber
+// @version      0.2.0
+// @description  Better chat for itslearning
+// @author       Drumber
+// @match        https://*.itslearning.com/*
+// @updateURL    https://raw.githubusercontent.com/Drumber/itslearning-UserScripts/master/itslearning-ImagesInChat.user.js
+// @downloadURL  https://raw.githubusercontent.com/Drumber/itslearning-UserScripts/master/itslearning-ImagesInChat.user.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// ==/UserScript==
+
+/* Config stuff */
+// register menu command
+GM_registerMenuCommand('Script Settings', () => {
+    // open GM_config dialog
+    GM_config.open();
+});
+
+// initialize and construct config
+GM_config.init(
+{
+    'id': 'Settings',
+    'title': 'Script Settings',
+    'fields':
+    {
+        'image-viewer':
+        {
+            'label': 'Enable image viewer',
+            'type': 'checkbox',
+            'default': 'true'
+        }
+    },
+    'events': // callback functions
+    {
+        'open': function() { GM_config.frame.setAttribute('style', 'width: 300px; height: 500px; inset: 20px 20px auto auto; z-index: 9999; position: fixed; overflow: auto; max-width: 95%; max-height: 95%;'); }
+    },
+    'css': '#Settings { background: #212121 !important; color: #ffffff !important; } #Settings .reset { color: #ffffff !important; }'
+});
+
+
+(function() {
+    'use strict';
+    console.log("Images-In-Chat script is activated...");
+    // trigger function when a message element was found
+    waitForKeyElements(".c-messages__attachment .c-messages__attachment-content", processMessageElement)
+})();
+
+function processMessageElement(jNode) {
+    // expects div with class ".c-messages__header-thread_name"
+    var msg = jNode[0];
+    // find anchor child element
+    var imgAnchor = msg.getElementsByTagName("a")[0];
+    // check if attachment anchor element exists and if
+    // there is not already an added img element
+    if(imgAnchor && !msg.getElementsByTagName("img")[0]) {
+        // try to get attachment url
+        var imgUrl = imgAnchor.href;
+        if(imgUrl) {
+            // remove "Download=1" attribute
+            imgUrl = imgUrl.replace("&Download=1");
+            // create new img element
+            var img = document.createElement("img");
+            img.src = imgUrl;
+            // TODO: maybe reduce image size
+            // set onclick event if image-viewer is enabled
+            if(GM_config.get('image-viewer')) {
+                img.onclick = function() {
+                    showImage(this);
+                }
+            }
+            // add img elemt to parent of message
+            msg.parentElement.appendChild(img);
+            //console.log("Found and added image to message.", imgUrl);
+            // remove img on invalid image type
+            img.onerror = function() {
+                msg.parentElement.removeChild(img);
+                //console.log("Removed invalid image.", img);
+            }
+        }
+    }
+}
+
+function showImage(eventImage) {
+    var img = eventImage.cloneNode(true); // clone image element
+
+    // remove any existing dialog
+    var existingDialog = document.getElementById("custom-image-viewer");
+    if(existingDialog) {
+        document.body.removeChild(existingDialog);
+    }
+
+    var dialog = document.createElement("div");
+    dialog.id = "custom-image-viewer";
+    var ds = dialog.style; // dialog style
+    ds.overflow = "hidden";
+    ds.display = "block";
+    ds.position = "fixed";
+    ds.left = "50%";
+    ds.top = "50%";
+    ds.transform = "translate(-50%, -50%)";
+    ds.zIndex = "9999";
+    ds.maxWidth = "95%";
+    ds.maxHeight = "95%";
+    ds.background = "rgba(10,10,10,0.8)";
+    ds.padding = "10px";
+    ds.borderRadius = "10px";
+
+    // get actual image size
+    var w = img.naturalWidth;
+    var h = img.naturalHeight;
+
+    var ims = img.style; // image style
+    ims.maxWidth = "none";
+    ims.cursor = "zoom-in";
+    // set width and height of the image and dialog
+    ims.width = `${w}px`;
+    ims.height = `${h}px`;
+    ds.width = `${w + 40}px`;
+    ds.height = `${h + 60}px`;
+
+    var imgWrapper = document.createElement("div");
+    imgWrapper.style.cssText = "overflow: auto; height: 95%; width: 100%;";
+    imgWrapper.appendChild(img);
+
+    // close button
+    var closeBtn = document.createElement("button");
+    closeBtn.innerHTML = "&#x2715;";
+    closeBtn.style.cssText = "color: #ffffff; font-size: 24px; position: absolute; right: 8px; top: 8px; background: 0; border: none;";
+    closeBtn.position = "absolute";
+    closeBtn.onclick = function() {
+        document.body.removeChild(dialog); // remove dialog
+    }
+    var dialogHead = document.createElement("div");
+    dialogHead.style.width = "100%";
+    dialogHead.style.height = "30px";
+    dialogHead.appendChild(closeBtn);
+
+    dialog.appendChild(dialogHead);
+    dialog.appendChild(imgWrapper);
+
+    // add zoom on image click
+    img.onclick = function() {
+        if(ims.width != `${w}px`) {
+            // reset image size
+            ims.width = `${w}px`;
+            ims.height = `${h}px`;
+        } else {
+            var scaleFactor = 2;
+            //ims.transform = `scale(${scaleFactor})`;
+            ims.width = `${w * scaleFactor}px`;
+            ims.height = `${h * scaleFactor}px`;
+
+            img.addEventListener('mousemove', function moveListener(e) {
+                // remove listener
+                if(ims.width == `${w}px`) {
+                    img.removeEventListener('mousemove', moveListener);
+                    return;
+                }
+                // scroll to mouse position
+                var x = e.clientX + 200;
+                var y = e.clientY + 200;
+                if(e.clientX < window.innerWidth / 2) {
+                    x = e.clientX - 200;
+                }
+                if(e.clientY < window.innerHeight / 2) {
+                    y = e.clientY - 200;
+                }
+                // TODO: improve scroll
+                imgWrapper.scrollTo(x, y);
+            });
+        }
+    }
+
+    // animate if supported by browser
+    if(dialog.animate) {
+        dialog.animate([
+            { transform: 'translate(-50%, -50%) scale(0.5)', opacity: '0.75' },
+            { transform: 'translate(-50%, -50%) scale(1)', opacity: '1' }
+            ], {
+            duration: 200
+        });
+    }
+
+    // show dialog
+    document.body.appendChild(dialog);
+
+    // add close listener when clicked outside
+    window.onclick = function(event) {
+        // check if clicked element is a descendant of the dialog
+        if(dialog.contains(event.target) == false) {
+            try {
+                document.body.removeChild(dialog);
+            } catch(error) {}
+        }
+    }
+}
