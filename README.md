3ba7a82bbc52a4b7f0a5054228598cf27cce9f02
Lars <29163322+Drumber@users.noreply.github.com>
2022-01-14 18:07:13 +0100

    Added MiddleClick user script

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # itslearning-UserScripts
 UserScripts to improve the experience on itslearning.
-- ImagesInChat: adds a image preview to the chat
+- ImagesInChat: adds a image preview to the chat and support drag-and-drop for uploading files
+- MiddleClick: fixes the middle-click action for opening itslearning pages in a new tab
 
 ## Installtion
 1. Install [Tampermonkey for Firefox](https://tampermonkey.net/?ext=dhdg&browser=firefox), [Chrome](https://tampermonkey.net/?ext=dhdg&browser=chrome), [Opera](https://tampermonkey.net/?ext=dhdg&browser=opera) or [Safari](https://tampermonkey.net/?ext=dhdg&browser=safari).
-2. Install [itslearning-ImagesInChat.user.js](https://raw.githubusercontent.com/Drumber/itslearning-UserScripts/master/itslearning-ImagesInChat.user.js)
+2. Install the user scripts you want:
+    - [itslearning-ImagesInChat.user.js](https://raw.githubusercontent.com/Drumber/itslearning-UserScripts/master/itslearning-ImagesInChat.user.js)
+    - [itslearning-MiddleClick.user.js](https://raw.githubusercontent.com/Drumber/itslearning-UserScripts/master/itslearning-MiddleClick.user.js)
 
 ##
 Check out my [Dark-Theme for itslearning](https://github.com/Drumber/Dark-itslearning).
---
+++ itslearning-MiddleClick.user.js
@@ -0,0 +1,100 @@
+// ==UserScript==
+// @name         MiddleClick
+// @namespace    https://github.com/Drumber
+// @version      0.1.0
+// @description  Middle click iframe content
+// @author       Drumber
+// @match        https://*.itslearning.com/*
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant        GM_openInTab
+// @noframes
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("MiddleClick script is activated...");
+    processQueryParams();
+    setupClickEventInterceptor(window);
+    waitForKeyElements("iframe", (jNode) => {
+        const iframe = jNode[0]
+        setupClickEventInterceptor(iframe.contentWindow, iframe.id);
+    });
+})();
+
+/**
+ * Check if there are query params added by this user script (prefixed by mcf-)
+ * and insert the specified content url into the target iframe.
+ */
+function processQueryParams() {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has("mcf-iframe") && urlParams.has("mcf-url")) {
+        const iframeId = urlParams.get("mcf-iframe");
+        const contentUrl = urlParams.get("mcf-url");
+        const iframe = document.getElementById(iframeId)
+        if (iframe) {
+            console.log("[MCF] Loading URL", contentUrl, "into iframe", iframe);
+            iframe.src = contentUrl
+        }
+    }
+}
+
+/**
+ * Adds an auxclick event listener to the given window element for intercepting
+ * middle click events. Set iframeId to the ID of the iframe the given window
+ * element belongs to, or leave it undefined if the listener is added to the
+ * root document.
+ */
+function setupClickEventInterceptor(windowEl, iframeId) {
+    // source: https://stackoverflow.com/a/62311399/12821118
+    windowEl.addEventListener("auxclick", (event) => {
+        console.log(event);
+        if (event.button === 1 && shouldBlockActionForElement(event.target)) {
+            event.preventDefault();
+            handleMiddleClick(event.target, iframeId);
+        }
+    });
+}
+
+/**
+ * Checks if the target anchor element refers to an external website or to a
+ * itslearning subpage.
+ */
+function shouldBlockActionForElement(el) {
+    console.log("check", el);
+    if (el.nodeName == "A") {
+        return el.hostname == location.hostname;
+    }
+    const closestAnchor = el.closest("a");
+    return closestAnchor && shouldBlockActionForElement(closestAnchor);
+}
+
+function handleMiddleClick(el, iframeId) {
+    if (!iframeId && el.target) {
+        // itslearning uses the target attribute on anchor tags to load the content in the target iframe
+        // e.g. <a href="..." target="mainmenu"> targets iframe <iframe id="..." name="mainmenu">
+        const target = el.target;
+        iframeId = document.querySelector(`[name=${target}]`).id;
+    }
+    const contentUrl = el.href;
+    if (iframeId && contentUrl) {
+        cloneWindowForIFrame(iframeId, contentUrl);
+    } else {
+        // backup: normal behaviour
+        openTab(contentUrl);
+    }
+}
+
+function cloneWindowForIFrame(iframeId, contentUrl) {
+    const url = new URL(location.href);
+    url.searchParams.set("mcf-iframe", iframeId);
+    url.searchParams.set("mcf-url", contentUrl);
+    openTab(url.href);
+}
+
+function openTab(url) {
+    GM_openInTab(url, {
+        active: false,
+        insert: true,
+        setParent: true
+    });
+}
